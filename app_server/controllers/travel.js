b6feed03f0f5e3658cdc7ee05696ceb9d608420e
plastine <aj77794@gmail.com>
2026-03-29 18:50:19 -0400

    Module 3 baseline completed

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,11 @@
+
+/*for parsing in the data file (structured as JSON)*/
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
+/* GET travel view */
 const travel = (req,res) => {
-    res.render('travel', {title: 'Travlr Getaways'});
+    res.render('travel', {title: 'Travlr Getaways', trips});
 };
 
     module.exports = {
